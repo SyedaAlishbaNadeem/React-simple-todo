32434980eecf43810d4ba400c94335cb4a4bea9c
SyedaAlishbaNadeem <s.alishba.nadeem@gmail.com>
2022-09-27 16:51:04 +0500

    first js

---
+++ myapp/src/component/Button.js
@@ -0,0 +1,12 @@
+// function Button(props) {
+//     console.log(props);
+//     return <button className= {`btn ${props.className}`} > {props.btnvalue} </button>
+
+// }
+// export default Button;
+
+function Button(props){
+    return <button onClick={props.func} className= {props.class}>{props.btnvalue}</button>
+
+}
+export default Button
